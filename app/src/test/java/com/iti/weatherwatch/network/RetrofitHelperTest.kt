7c229b56c367054dc3f50e472745692b36c29f9d
AHMHAM <ahmedhambouta@gmail.com>
2023-03-29 16:17:21 +0200

    test: retrofit unit test for open weather api

---
+++ app/src/test/java/com/iti/weatherwatch/network/RetrofitHelperTest.kt
@@ -0,0 +1,27 @@
+package com.iti.weatherwatch.network
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.iti.weatherwatch.model.OpenWeatherApi
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertNotNull
+import org.junit.Rule
+import org.junit.Test
+import retrofit2.Response
+
+//@RunWith(AndroidJUnit4::class)
+class RetrofitHelperTest {
+    @get:Rule
+    val instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @Test
+    fun getCurrentWeather_latAndLong_returnOpenWeatherApiModelHasValue() {
+        val lat = 31.19264
+        val long = 29.9060892
+        val retrofitHelper = RetrofitHelper
+        var result: Response<OpenWeatherApi>?
+        runBlocking {
+            result = retrofitHelper.getCurrentWeather(lat, long)
+            assertNotNull(result?.body())
+        }
+    }
+}
