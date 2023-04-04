b1cdef02d9f1a4806c97507228f3f0bbed1aaac4
AHMHAM <ahmedhambouta@gmail.com>
2023-04-04 22:41:48 +0200

    feat: add settings screen that allows the user to change language & temperature unit

---
+++ app/src/main/java/com/iti/weatherwatch/settings/viewmodel/SettingsViewModel.kt
@@ -0,0 +1,6 @@
+package com.iti.weatherwatch.settings.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class SettingsViewModel(app: Application) : AndroidViewModel(app)
