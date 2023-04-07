f915874abf4ae558a361bdf03c98abc72335ec2b
AHMHAM <ahmedhambouta@gmail.com>
2023-04-07 16:39:07 +0200

    feat: add ability to choose a favorite location from the map and view its weather.

--- app/src/main/java/com/iti/weatherwatch/favorites/view/FavoritesFragment.kt
+++ app/src/main/java/com/iti/weatherwatch/favorites/view/FavoritesFragment.kt
@@ -2,15 +2,27 @@ package com.iti.weatherwatch.favorites.view
 
 import android.os.Bundle
 import android.view.*
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.iti.weatherwatch.R
 import com.iti.weatherwatch.databinding.FragmentFavoritesBinding
+import com.iti.weatherwatch.datasource.WeatherRepository
 import com.iti.weatherwatch.favorites.viewmodel.FavoritesViewModel
+import com.iti.weatherwatch.favorites.viewmodel.FavoritesViewModelFactory
+import com.iti.weatherwatch.model.OpenWeatherApi
 
 class FavoritesFragment : Fragment() {
 
     private var _binding: FragmentFavoritesBinding? = null
+    private val viewModel: FavoritesViewModel by viewModels {
+        FavoritesViewModelFactory(WeatherRepository.getRepository(requireActivity().application))
+    }
+
+    private lateinit var favoriteAdapter: FavoriteAdapter
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -21,21 +33,61 @@ class FavoritesFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val dashboardViewModel =
-            ViewModelProvider(this).get(FavoritesViewModel::class.java)
-
         _binding = FragmentFavoritesBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+        return binding.root
+    }
 
-        val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        handleBackButton()
+
+        initFavoritesRecyclerView()
+
+        viewModel.getFavorites()
+
+        binding.floatingActionButton.setOnClickListener {
+            val action = FavoritesFragmentDirections.actionNavigationDashboardToMapsFragment(true)
+            findNavController().navigate(action)
         }
-        return root
+
+        lifecycleScope.launchWhenStarted {
+            viewModel.favorites.collect {
+                if (!it.isNullOrEmpty()) {
+                    binding.textEmptyList.visibility = View.GONE
+                }
+                fetchFavoritesRecycler(it)
+            }
+        }
+    }
+
+    private fun fetchFavoritesRecycler(list: List<OpenWeatherApi>?) {
+        favoriteAdapter.favoriteList = list ?: emptyList()
+        favoriteAdapter.notifyDataSetChanged()
+    }
+
+    private fun initFavoritesRecyclerView() {
+        val linearLayoutManager = LinearLayoutManager(FavoritesFragment().context)
+        favoriteAdapter = FavoriteAdapter(this.requireContext(), viewModel)
+        binding.favoriteRecyclerView.layoutManager = linearLayoutManager
+        binding.favoriteRecyclerView.adapter = favoriteAdapter
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+
+    private fun handleBackButton() {
+        binding.root.isFocusableInTouchMode = true
+        binding.root.requestFocus()
+        binding.root.setOnKeyListener(View.OnKeyListener { v, keyCode, event ->
+            if (keyCode == KeyEvent.KEYCODE_BACK && event.action == KeyEvent.ACTION_UP) {
+                Navigation.findNavController(v)
+                    .navigate(R.id.action_navigation_dashboard_to_navigation_home)
+                return@OnKeyListener true
+            }
+            return@OnKeyListener false
+        })
+    }
+
 }
---
+++ app/src/main/java/com/iti/weatherwatch/favorites/viewmodel/DisplayFavoriteWeatherViewModel.kt
@@ -0,0 +1,45 @@
+package com.iti.weatherwatch.favorites.viewmodel
+
+import androidx.lifecycle.*
+import com.iti.weatherwatch.datasource.WeatherRepository
+import com.iti.weatherwatch.model.OpenWeatherApi
+import kotlinx.coroutines.*
+
+class DisplayFavoriteWeatherViewModel(private val repository: WeatherRepository) : ViewModel() {
+    fun getWeather(id: Int) {
+        var result: OpenWeatherApi? = null
+        val job = viewModelScope.launch(Dispatchers.IO) {
+            result = repository.getFavoriteWeather(id)
+        }
+        viewModelScope.launch(Dispatchers.Main) {
+            job.join()
+            job.cancel()
+            _weather.postValue(result)
+            this.cancel()
+        }
+    }
+
+    fun updateWeather(
+        latitude: Double,
+        longitude: Double,
+        units: String,
+        language: String,
+        id: Int
+    ) {
+        var result: OpenWeatherApi? = null
+        val job = viewModelScope.launch(Dispatchers.IO) {
+            result =
+                repository.updateFavoriteWeather("$latitude", "$longitude", units, language, id)
+        }
+        viewModelScope.launch(Dispatchers.Main) {
+            job.join()
+            job.cancel()
+            _weather.postValue(result)
+            this.cancel()
+        }
+    }
+
+    private var _weather = MutableLiveData<OpenWeatherApi?>()
+    val weather = _weather
+
+}
