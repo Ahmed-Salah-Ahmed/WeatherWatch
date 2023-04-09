5cbfdbbfaa6673200ee88c88542403cf15ba5547
AHMHAM <ahmedhambouta@gmail.com>
2023-04-09 22:29:19 +0200

    test: update retrofit & test using mockito

--- app/src/test/java/com/iti/weatherwatch/datasource/remote/RetrofitHelperTest.kt
+++ app/src/test/java/com/iti/weatherwatch/datasource/remote/RetrofitHelperTest.kt
@@ -1,9 +1,9 @@
-package com.iti.weatherwatch.network
+package com.iti.weatherwatch.datasource.remote
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.iti.weatherwatch.datasource.model.OpenWeatherApi
-import com.iti.weatherwatch.datasource.remote.RetrofitHelper
 import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertEquals
 import org.junit.Assert.assertNotNull
 import org.junit.Rule
 import org.junit.Test
@@ -18,11 +18,26 @@ class RetrofitHelperTest {
     fun getCurrentWeather_latAndLong_returnOpenWeatherApiModelHasValue() {
         val lat = "31.19264"
         val long = "29.9060892"
+        val language = "en"
+        val units = "metric"
         val retrofitHelper = RetrofitHelper
         var result: Response<OpenWeatherApi>?
         runBlocking {
-            result = retrofitHelper.getCurrentWeather(lat, long, "en", "metric")
+            result = retrofitHelper.getCurrentWeather(lat, long, language, units)
             assertNotNull(result?.body())
         }
+    }
+
+    @Test
+    fun testGetCurrentWeather() = runBlocking {
+        val response = RetrofitHelper.getCurrentWeather(
+            lat = "31.19264",
+            long = "29.9060892",
+            language = "en",
+            units = "metric"
+        )
+        assertEquals(true, response.isSuccessful)
+        val weather = response.body()
+        assertEquals(OpenWeatherApi::class.java, weather?.javaClass)
     }
 }
